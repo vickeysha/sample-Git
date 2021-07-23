c7a612880f533518f42d5dac4576eaaa7b8314df
vickeysha <vickeysha36@gmail.com>
2021-07-23 09:49:32 +0530

    sample

--- src/org/test/Actions.java
+++ src/org/test/Actions.java
@@ -14,9 +14,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Actions {
 	
 	 WebDriver driver;
-
+	public static void method1() {
+		// TODO Auto-generated method stub
+		System.out.println("method1");
+	}
 
 	public static void main(String[] args) throws InterruptedException   {
+		method1();
      System.setProperty("webdriver.chrome.driver","C:\\Users\\Venkatesh\\Documents\\Selenium Browser Drivers\\chromedriver.exe");
      
      WebDriver driver=new ChromeDriver();
